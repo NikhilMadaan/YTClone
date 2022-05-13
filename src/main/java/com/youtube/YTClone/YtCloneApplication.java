b510282be2c9132b95d7a55c242dba5c685cd21f
NikhilMadaan <nikhildeveloper31@gmail.com>
2022-05-13 19:03:53 +0530

    Initial commit

---
+++ src/main/java/com/youtube/YTClone/YtCloneApplication.java
@@ -0,0 +1,13 @@
+package com.youtube.YTClone;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YtCloneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YtCloneApplication.class, args);
+	}
+
+}
