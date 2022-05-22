60fa531cf8ac87fadbff9c9983180997578d0fc6
NikhilMadaan <nikhildeveloper31@gmail.com>
2022-05-22 15:18:17 +0530

    nikhil | Upload thumbnail to AWS

--- frontend/youtube-clone-ui/src/app/video.service.ts
+++ frontend/youtube-clone-ui/src/app/video.service.ts
@@ -17,6 +17,16 @@ export class VideoService {
     //HTTP post call
     return this.httpClient.post<UploadVideoResponse>("http://localhost:8080/api/videos",formData);
 
+  }
+  uploadThumbnail(fileEntry: File,videoId:string):Observable<String>{
+    //check upload video component ts for reference
+    const formData = new FormData()
+    formData.append('file', fileEntry, fileEntry.name)
+    formData.append('videoId', videoId);
+    //HTTP post call
+    return this.httpClient.post("http://localhost:8080/api/videos/thumbnail",formData,{
+      responseType:'text'
+    });
 
   }
 }
