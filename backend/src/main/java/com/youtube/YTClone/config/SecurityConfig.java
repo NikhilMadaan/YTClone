602b67e978e426fe2713f390d5a57d3205104d6e
NikhilMadaan <nikhildeveloper31@gmail.com>
2022-05-25 19:58:45 +0530

    nikhil | Auth0 implemented springboot CORS policy addded =

--- backend/src/main/java/com/youtube/YTClone/config/SecurityConfig.java
+++ backend/src/main/java/com/youtube/YTClone/config/SecurityConfig.java
@@ -2,7 +2,10 @@ package com.youtube.YTClone.config;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.oauth2.core.DelegatingOAuth2TokenValidator;
@@ -13,6 +16,8 @@ import org.springframework.security.oauth2.jwt.JwtDecoders;
 import org.springframework.security.oauth2.jwt.JwtValidators;
 import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
 
+@Configuration
+@EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter  {
 
     @Value("${spring.security.oauth2.resourceserver.jwt.issuer-uri}")
@@ -28,6 +33,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter  {
                 .sessionManagement()
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and()
+                .cors(Customizer.withDefaults())
                 .oauth2ResourceServer()
                 .jwt();
 
