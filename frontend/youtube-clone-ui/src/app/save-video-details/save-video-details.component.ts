17c49ee6c8664cebc92b822ab809e68474747ddb
NikhilMadaan <nikhildeveloper31@gmail.com>
2022-05-25 18:48:24 +0530

    nikhil | Auth0 implemented

--- frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
+++ frontend/youtube-clone-ui/src/app/save-video-details/save-video-details.component.ts
@@ -5,6 +5,7 @@ import {COMMA, ENTER} from "@angular/cdk/keycodes";
 import {ActivatedRoute} from "@angular/router";
 import {VideoService} from "../video.service";
 import {MatSnackBar} from "@angular/material/snack-bar";
+import {VideoDto} from "../VideoDto";
 
 @Component({
   selector: 'app-save-video-details',
@@ -22,6 +23,8 @@ export class SaveVideoDetailsComponent implements OnInit {
   selectedFileName='';
   fileSelected=false;
   videoUrl!:string
+  thumbnailUrl!:string
+
   readonly separatorKeysCodes = [ENTER, COMMA] as const;
   tags: string[] = [];
    videoId: '';
@@ -30,6 +33,7 @@ export class SaveVideoDetailsComponent implements OnInit {
     this.videoId= this.activatedRoute.snapshot.params['videoId'];
     this.videoService.getVideo(this.videoId).subscribe(data=>{
       this.videoUrl=data.videoUrl;
+      this.thumbnailUrl=data.thumbnailUrl
     })
     this.saveVideoDetails=new FormGroup({
       title:this.title,
@@ -75,6 +79,25 @@ export class SaveVideoDetailsComponent implements OnInit {
       console.log(data);
       //show success notification
       this._snackBar.open("Thumbnail added successfully!!","OK");
+    })
+  }
+
+  saveVideo() {
+    //make call to video service to make a http call to backend
+    const videoMetadata:VideoDto={
+      "id":this.videoId,
+      "title":this.saveVideoDetails.get('title')?.value,
+      "description":this.saveVideoDetails.get('description')?.value,
+      "tags":this.saveVideoDetails.get('tags')?.value,
+      "videoStatus":this.saveVideoDetails.get('videoStatus')?.value,
+      "videoUrl":this.videoUrl,
+      "thumbnailUrl":this.thumbnailUrl
+
+    }
+    console.log(videoMetadata)
+    this.videoService.saveVideo(videoMetadata).subscribe(data=>{
+      console.log(data);
+      this._snackBar.open("Metadata Saved","Ok")
     })
   }
 }
