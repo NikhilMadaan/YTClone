1ac1f4e7ff7f4068acccdbe52cc16a0230e8ec93
NikhilMadaan <nikhildeveloper31@gmail.com>
2022-05-26 19:01:48 +0530

    nikhil | Auth0 implemented angular =

--- frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
+++ frontend/youtube-clone-ui/src/app/auth/auth-config.module.ts
@@ -1,19 +1,25 @@
-import { NgModule } from '@angular/core';
-import { AuthModule } from 'angular-auth-oidc-client';
+import {NgModule} from '@angular/core';
+import {AuthModule} from 'angular-auth-oidc-client';
 
 
 @NgModule({
-    imports: [AuthModule.forRoot({
-        config: {
+  imports: [AuthModule.forRoot({
+    config: {
             authority: 'https://dev-3cm2yz0q.us.auth0.com',
-            redirectUrl: window.location.origin,
+      redirectUrl: window.location.origin,
             clientId: 'kdjgMQGqOscmGSv1q7tp5IctfTEfqFPb',
-            scope: 'openid profile offline_access',
-            responseType: 'code',
-            silentRenew: true,
-            useRefreshToken: true,
-        }
-      })],
-    exports: [AuthModule],
+      scope: 'openid profile offline_access',
+      responseType: 'code',
+      silentRenew: true,
+      useRefreshToken: true,
+      secureRoutes: ['http://localhost:8080/'],
+      customParamsAuthRequest: {
+        audience: 'http://localhost:8080/'
+      }
+    }
+  })],
+  providers: [],
+  exports: [AuthModule],
 })
-export class AuthConfigModule {}
+export class AuthConfigModule {
+}
